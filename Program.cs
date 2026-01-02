8e586cf56c03b7c99cc9fb435335f8b7771de474
Rahul Poddar <poddarrahul58@gmail.com>
2026-01-02 19:38:23 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+public class Employee
+{
+
+    public Employee()
+    {
+        // Constructor same name is class name excuted when object is called or executed 
+    }
+    public int _experience; /// variable or you say this is property
+    public int Experience
+    {
+        get
+        {
+            return _experience;
+        }
+        set
+        {
+            _experience = value;
+        }
+    }
+}
+
+
+public class program 
+{
+    public static void main(string[] args)
+    {
+
+    }
+}
+
+
