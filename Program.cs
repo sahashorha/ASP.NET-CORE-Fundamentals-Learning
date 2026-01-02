b1c32a2348b85476c3ee77d8de00f737a3ab2dfc
Rahul Poddar <poddarrahul58@gmail.com>
2026-01-02 21:08:46 +0530

    Intial commit

--- Program.cs
+++ Program.cs
@@ -3,31 +3,28 @@
 
 public class Employee
 {
-
     public Employee()
     {
         // Constructor same name is class name excuted when object is called or executed 
     }
     public int _experience; /// variable or you say this is property
-    public int Experience
+    public int Experience { get; set; }
+    public void CalculateSalary()
     {
-        get
-        {
-            return _experience;
-        }
-        set
-        {
-            _experience = value;
-        }
+        int salary = _experience * 300000;
+        Console.WriteLine("Salary= {0} ",salary);
     }
 }
 
 
 public class program 
 {
-    public static void main(string[] args)
+    public static void Main(string[] args)
     {
-
+        Employee employee = new Employee();
+        employee.Experience = 3;
+        employee.CalculateSalary();
+        Console.ReadLine();
     }
 }
 
